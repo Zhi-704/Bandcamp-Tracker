6125b5757d3fa4a253a95d2ff0e59d9aa5c77a82
Zhi-704 <zhi.liang.704@gmail.com>
2024-06-20 10:29:42 +0100

    Changed requirements, added async to extract, polished pylint score to above 9

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,18 +1,22 @@
 '''File used for extracting information from bandcamp's website and API'''
 
+from time import sleep
 import json
 import logging
+import asyncio
+import aiohttp
 import requests as req
 from bs4 import BeautifulSoup
 
 
 BANDCAMP_SALES_URL = "https://bandcamp.com/api/salesfeed/1/get_initial"
-MAX_TIMEOUT_SECONDS = 10
+MAX_TIMEOUT_SECONDS = 20
 
 
-def get_data_from_url(site_url: str = BANDCAMP_SALES_URL,
-                      max_timeout: int = MAX_TIMEOUT_SECONDS) -> dict:
-    '''Get content from the specified URL'''
+def get_sale_data_from_api(site_url: str = BANDCAMP_SALES_URL,
+                           max_timeout: int = MAX_TIMEOUT_SECONDS) -> dict:
+    '''Get content from the specified api endpoint'''
+    sleep(10)
     try:
         response = req.get(site_url, timeout=max_timeout)
 
@@ -20,7 +24,7 @@ def get_data_from_url(site_url: str = BANDCAMP_SALES_URL,
             response_contents = response.json()
 
             return response_contents
-        logging.error("Failed to retrieve data. HTTP Status code: %s",
+        logging.error("Failed to retrieve sale data from API. HTTP Status code: %s",
                       response.status_code)
 
     except req.exceptions.RequestException as e:
@@ -30,52 +34,12 @@ def get_data_from_url(site_url: str = BANDCAMP_SALES_URL,
     return None
 
 
-def extract_list_of_items(event_list: list[dict]) -> list[dict]:
-    '''Extract all items from event list, where each element is an item'''
-    item_list = []
-    counter = 0
-    for event in event_list:
-        if event['event_type'] != 'sale':
-            continue
-        if len(event['items']) < 1:
-            continue
-
-        for item in event['items']:
-            if not (item["item_type"] == "a" or item["item_type"] == "t"):
-                continue
-            item_list.append(extract_and_scrape_item(item))
-            print("Scraped: ", counter)
-            counter += 1
-
-    return item_list
-
-
-def extract_and_scrape_item(purchase_dict: dict) -> dict:
-    '''Scrape and extract information for an item, giving
-    more complete information for the item'''
-    if purchase_dict["item_type"] == "t":
-        purchase_dict["track_tags"] = scrape_tags(
-            insert_protocol_url(purchase_dict["url"]))
-    if purchase_dict["item_type"] == "a":
-        purchase_dict["album_tags"] = scrape_tags(
-            insert_protocol_url(purchase_dict["url"]))
-    if purchase_dict["item_type"] == "t" and purchase_dict["album_title"] is not None:
-        stem_url = get_stem_url(insert_protocol_url(purchase_dict["url"]))
-        album_url = scrape_album_url(insert_protocol_url(purchase_dict["url"]))
-
-        if stem_url is not None and album_url is not None:
-            purchase_dict["album_url"] = stem_url+album_url
-            purchase_dict["album_tags"] = scrape_tags(stem_url+album_url)
-
-    return purchase_dict
-
-
-def save_to_json(list_to_be_written: list) -> None:
+def save_to_json(list_to_be_written: list, filename: str) -> None:
     '''Saves list of dictionary to json'''
-    with open('sanity_check.json', 'w', encoding='utf-8') as file:
+    with open(filename, 'w', encoding='utf-8') as file:
         json.dump(list_to_be_written, file, indent=4)
 
-    logging.info("List of dictionaries has been saved to sanity_check.json")
+    logging.info("List of dictionaries has been saved to %s", filename)
 
 
 def insert_protocol_url(url_string: str) -> str:
@@ -91,18 +55,82 @@ def get_stem_url(url_string: str) -> str:
     return "/".join(parts)
 
 
-def scrape_album_url(webpage_url: str) -> str:
-    '''Scrapes album url of a specified webpage url'''
+async def extract_list_of_items(event_list: list[dict],
+                                timeout: int = MAX_TIMEOUT_SECONDS) -> list[dict]:
+    '''Extract all items from event list, where each element is an item'''
+    item_list = []
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for event in event_list:
+            if event['event_type'] != 'sale':
+                continue
+            if len(event['items']) < 1:
+                continue
+
+            for item in event['items']:
+                if not (item["item_type"] == "a" or item["item_type"] == "t"):
+                    continue
+                tasks.append(extract_and_scrape_item(session, item, timeout))
+
+        item_list = await asyncio.gather(*tasks)
+
+    return item_list
+
+
+async def extract_and_scrape_item(session: aiohttp.ClientSession,
+                                  purchase_dict: dict,
+                                  timeout: int) -> dict:
+    '''Scrape and extract information for an item, giving
+    more complete information for the item'''
+    if purchase_dict["item_type"] == "t":
+        purchase_dict["track_tags"] = await scrape_tags(session,
+                                                        insert_protocol_url(
+                                                            purchase_dict["url"]),
+                                                        timeout)
+    if purchase_dict["item_type"] == "a":
+        purchase_dict["album_tags"] = await scrape_tags(session,
+                                                        insert_protocol_url(
+                                                            purchase_dict["url"]),
+                                                        timeout)
+    if purchase_dict["item_type"] == "t" and purchase_dict["album_title"] is not None:
+        stem_url = get_stem_url(insert_protocol_url(purchase_dict["url"]))
+        album_url = await scrape_album_url(session,
+                                           insert_protocol_url(
+                                               purchase_dict["url"]),
+                                           timeout)
+
+        if stem_url is not None and album_url is not None:
+            purchase_dict["album_url"] = stem_url + album_url
+            purchase_dict["album_tags"] = await scrape_tags(session, stem_url + album_url, timeout)
+
+    return purchase_dict
+
+
+async def fetch(session: aiohttp.ClientSession, specified_url: str, timeout: int):
+    '''Get text/html content from a specified url'''
     try:
-        response = req.get(webpage_url, timeout=MAX_TIMEOUT_SECONDS)
-    except req.exceptions.RequestException as e:
-        logging.error("A request error has occurred scraping album: %s", e)
-        return None
-    except TimeoutError as e:
-        logging.error("A timeout error has occurred scraping album: %s", e)
+        async with session.get(specified_url, timeout=timeout) as response:
+            if response.status == 200:
+                return await response.text()
+            logging.error("Failed to fetch data. HTTP Status code: %s",
+                          response.status)
+            logging.error("The error specified url is: %s", specified_url)
+            return None
+    except aiohttp.ClientError as e:
+        logging.error("A fetch request error has occurred: %s", e)
+    except asyncio.TimeoutError as e:
+        logging.error("A fetch timeout error has occurred: %s", e)
+    return None
+
+
+async def scrape_album_url(session: aiohttp.ClientSession, webpage_url: str, timeout: int) -> str:
+    '''Scrapes album url of a specified webpage url'''
+    html = await fetch(session, webpage_url, timeout)
+
+    if html is None:
         return None
 
-    soup = BeautifulSoup(response.text, features="html.parser")
+    soup = BeautifulSoup(html, features="html.parser")
     link = soup.find("a", id="buyAlbumLink")
 
     if link:
@@ -113,37 +141,58 @@ def scrape_album_url(webpage_url: str) -> str:
     return None
 
 
-def scrape_tags(webpage_url: str) -> list:
+async def scrape_tags(session: aiohttp.ClientSession, webpage_url: str, timeout: int) -> list:
     '''Scrapes tags of a specified webpage url'''
-    try:
-        response = req.get(webpage_url, timeout=MAX_TIMEOUT_SECONDS)
-    except req.exceptions.RequestException as e:
-        logging.error("A request error has occurred scraping tags: %s", e)
-        return
-    except TimeoutError as e:
-        logging.error("A timeout error has occurred scraping tags: %s", e)
-        return
+    html = await fetch(session, webpage_url, timeout)
+    if html is None:
+        return None
 
-    tag_list = []
-
-    soup = BeautifulSoup(response.text, features="html.parser")
+    soup = BeautifulSoup(html, 'html.parser')
     tags = soup.find_all("a", class_="tag")
-
-    for tag in tags:
-        tag_list.append(tag.text)
-
-    return tag_list if len(tag_list) > 0 else None
+    return [tag.text for tag in tags] if len(tags) > 0 else None
 
 
-if __name__ == "__main__":
+def configure_log() -> None:
+    '''Configures log output'''
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
-    logging.info("Testing logging")
 
-    data = get_data_from_url()
+def get_sales_data() -> list[dict]:
+    '''Get the latest sales data from bandcamp'''
+
+    logging.info("Extraction started")
+
+    sales_data = get_sale_data_from_api()
+    logging.info("Sales data gathered")
+    if sales_data is not None:
+        logging.info("Scraping begun")
+        list_of_albums_tracks = asyncio.run(
+            extract_list_of_items(sales_data['feed_data']['events']))
+        logging.info("Scraping ended")
+    else:
+        logging.info("Scraping did not initiate")
+        return
+    logging.info("Extract finished")
+
+    return list_of_albums_tracks
+
+
+if __name__ == "__main__":
+    configure_log()
+
+    logging.info("Extraction started")
+
+    data = get_sale_data_from_api()
+    logging.info("Sales data gathered")
     if data is not None:
-        list_of_items = extract_list_of_items(data['feed_data']['events'])
-        save_to_json(list_of_items)
+        logging.info("Scraping begun")
+        list_of_items = asyncio.run(
+            extract_list_of_items(data['feed_data']['events']))
+        save_to_json(list_of_items, "Checking_again.json")
+        logging.info("Scraping ended")
+    else:
+        logging.info("Scraping did not initiate")
+    logging.info("Extract finished")

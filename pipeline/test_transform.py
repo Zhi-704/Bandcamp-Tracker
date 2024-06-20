b80a7f3b571c3c51304512c7a63bcf3c8bcf69c6
eyuale <157725293+e-lemma@users.noreply.github.com>
2024-06-20 14:55:55 +0100

    add transform script and testing for script

--- pipeline/test_transform.py
+++ pipeline/test_transform.py
@@ -1,20 +1,152 @@
 """Tests for transform functions."""
 
-from transform import convert_unix_to_datetime, format_item_url, get_artist_url
-from datetime import datetime, timezone
+from transform import convert_unix_to_datetime, clean_data, transform_sales_data
+import pytest
 
 
 def test_convert_unix_to_datetime_valid():
+    """Tests that a valid Unix timestamp is converted to a string.."""
 
-    unix_timestamp = "1718801551.04217"
+    unix_timestamp = 1718801551.04217
 
-    assert convert_unix_to_datetime(
-        unix_timestamp) == datetime(2024, 6, 19, 12, 52, 31, tzinfo=timezone.utc)
+    assert convert_unix_to_datetime(unix_timestamp) == "2024-06-19 12:52:31"
 
 
-def test_convert_unix_to_datetime_valid():
+def test_convert_unix_to_datetime_invalid_type():
+    """Tests that a TypeError is raised if a float is not passed in."""
 
-    unix_timestamp = "1718801551.04217"
+    unix_timestamp = "171880151.04217"
 
-    assert convert_unix_to_datetime(
-        unix_timestamp) == datetime(2024, 6, 19, 12, 52, 31, tzinfo=timezone.utc)
+    with pytest.raises(TypeError) as exc_info:
+        convert_unix_to_datetime(unix_timestamp)
+        assert (
+            str(exc_info.value)
+            == "Expected a float for unix_timestamp, but got <class 'str'>"
+        )
+
+
+def test_convert_unix_to_datetime_invalid_unix():
+    """Tests that a ValueError is raised when a invalid Unix is passed in."""
+
+    unix_timestamp = -1718801551.04217
+
+    with pytest.raises(ValueError) as exc_info:
+        convert_unix_to_datetime(unix_timestamp)
+    assert str(exc_info.value) == "Unix timestamp cannot be negative."
+
+
+def test_convert_unix_to_datetime_invalid_datetime():
+    """Tests that a ValueError is raised if an 'impossible' unix is passed in."""
+
+    unix_timestamp = 9918801551.04217
+
+    with pytest.raises(ValueError) as exc_info:
+        convert_unix_to_datetime(unix_timestamp)
+    assert str(exc_info.value).startswith(
+        "The provided Unix timestamp corresponds to a future date or time: "
+    )
+
+
+def test_clean_data_valid():
+    """Tests that the correct keys are removed from the dictionary."""
+
+    input_data = {
+        "slug_type": "track",
+        "track_album_slug_text": "some_slug",
+        "artist": "Artist Name",
+        "track_title": "Song Title",
+        "currency": "USD",
+        "amount_paid": 1.99,
+        "country_code": "US",
+        "item_price": 1.29,
+        "art_id": 12345,
+        "releases": [{"id": 67890}],
+        "package_image_id": 98765,
+        "amount_paid_fmt": "$1.99",
+        "art_url": "https://example.com/image.jpg",
+        "amount_over_fmt": "$0.70",
+        "item_slug": "song-title",
+    }
+
+    expected_output = {
+        "artist": "Artist Name",
+        "track_title": "Song Title",
+    }
+
+    assert clean_data(input_data) == expected_output
+
+
+def test_clean_data_empty_input():
+    """Test that it performs as expected with an empty dictionary."""
+    input_data = {}
+    expected_output = {}
+
+    assert clean_data(input_data) == expected_output
+
+
+def test_clean_data_invalid():
+    """Test with invalid input type (not a dictionary)."""
+
+    input_data = "foobar"
+    with pytest.raises(TypeError):
+        clean_data(input_data)
+
+
+def test_transform_sales_data_valid():
+    """Tests that the expected, cleaned dictionary is returned."""
+
+    input_data = [
+        {
+            "utc_date": 1718891188.2938294,
+            "artist_name": "example_artist",
+            "item_type": "a",
+            "item_description": "Fine Art",
+            "album_title": None,
+            "slug_type": "a",
+            "track_album_slug_text": None,
+            "currency": "GBP",
+            "amount_paid": 4.99,
+            "item_price": 4.989999771118164,
+            "amount_paid_usd": 6.33,
+            "country": "Canada",
+            "art_id": 2365536250,
+            "releases": None,
+            "package_image_id": None,
+            "url": "//example_artist.bandcamp.com/album/fine-art",
+            "country_code": "ca",
+            "amount_paid_fmt": "£4.99",
+            "art_url": "https://example_artistcom/img/image.jpg",
+            "album_tags": [
+                "hip hop",
+                "hip-hop/rap",
+                "irish rap",
+                "punk",
+                "rap",
+                "Belfast",
+            ],
+        }
+    ]
+
+    expected_output = [
+        {
+            "utc_date": "2024-06-20 13:46:28",
+            "artist_name": "example_artist",
+            "item_type": "a",
+            "item_description": "Fine Art",
+            "album_title": None,
+            "amount_paid_usd": 6.33,
+            "country": "Canada",
+            "url": "https://example_artist.bandcamp.com/album/fine-art",
+            "album_tags": [
+                "hip hop",
+                "hip-hop/rap",
+                "irish rap",
+                "punk",
+                "rap",
+                "Belfast",
+            ],
+            "artist_url": "https://example_artist.bandcamp.com",
+        }
+    ]
+
+    assert transform_sales_data(input_data) == expected_output
--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,27 +1,81 @@
 """Script for transforming the data extracted from the api and web-scraping."""
 
 from datetime import datetime, timezone
+from extract import insert_protocol_url, get_stem_url, get_sales_data
 
 
-def convert_unix_to_datetime(unix_timestamp: str) -> datetime:
-    """Constructs and returns a datetime object from a unix timestamp."""
+def convert_unix_to_datetime(unix_timestamp: float) -> str:
+    """Constructs and returns a string representing date and time from a Unix timestamp."""
 
-    try:
-        unix = int(float(unix_timestamp))
-    except TypeError as e:
-        raise TypeError(f"{unix_timestamp} is not a valid unix timestamp!")
+    if not isinstance(unix_timestamp, float):
+        raise TypeError(
+            f"Expected a float for unix_timestamp, but got {
+                type(unix_timestamp)}"
+        )
 
-    return datetime.fromtimestamp(unix, tz=timezone.utc)
+    if unix_timestamp < 0:
+        raise ValueError("Unix timestamp cannot be negative.")
+
+    unix_timestamp_seconds = int(unix_timestamp)
+
+    date_time = datetime.fromtimestamp(unix_timestamp_seconds, tz=timezone.utc)
+
+    utc_date_time_now = datetime.now(timezone.utc)
+
+    if date_time > utc_date_time_now:
+        raise ValueError(
+            f"The provided Unix timestamp corresponds to a future date or time: {
+                date_time}"
+        )
+    return date_time.strftime("%Y-%m-%d %H:%M:%S")
 
 
-def format_item_url(item_url: str):
-    pass
+def clean_data(sales_data: dict) -> dict:
+    """Takes the sales data as a dict, and removes specific, unnecessary keys."""
+
+    if not isinstance(sales_data, dict):
+        raise TypeError("Input data must be a dictionary.")
+
+    keys_to_remove = [
+        "slug_type",
+        "track_album_slug_text",
+        "currency",
+        "amount_paid",
+        "item_price",
+        "art_id",
+        "releases",
+        "package_image_id",
+        "amount_paid_fmt",
+        "art_url",
+        "amount_over_fmt",
+        "item_slug",
+        "country_code",
+    ]
+
+    return {
+        key: value for key, value in sales_data.items() if key not in keys_to_remove
+    }
 
 
-def get_artist_url(item_url: str):
-    pass
+def transform_sales_data(sales_data: list[dict]) -> list[dict]:
+    """Cleans and formats the sales data, returning it as a list of dictionaries."""
+
+    cleaned_sales = []
+
+    for item in sales_data:
+
+        item["utc_date"] = convert_unix_to_datetime(item["utc_date"])
+        item["url"] = insert_protocol_url(item["url"])
+        item["artist_url"] = get_stem_url(item["url"])
+
+        cleaned_sales.append(clean_data(item))
+
+    return cleaned_sales
 
 
 if __name__ == "__main__":
 
-    print(convert_unix_to_datetime("1718801551.04217"))
+    list_of_items = get_sales_data()
+    print(list_of_items[0])
+    cleaned_data = transform_sales_data(list_of_items)
+    print(cleaned_data[0])

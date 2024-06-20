97f6e1511545ac9e6f0530151803d21da025d88e
Alina <trainee.alina.tariq@sigmalabs.co.uk>
2024-06-20 10:17:05 +0100

    basic querying functions

--- dashboard/database.py
+++ dashboard/database.py
@@ -22,6 +22,118 @@ def get_connection() -> Connection:
     return connection
 
 
+def get_popular_tracks(_conn: Connection, n: int = 5) -> pd.DataFrame:
+    """Returns the N most sold tracks in the database."""
+
+    print("Collating most popular tracks.")
+
+    query = """
+        SELECT T.title, A.name, COUNT(*) AS copies_sold
+        FROM track_purchase AS PT
+        JOIN track AS T
+        USING(track_id)
+        JOIN artist as A
+        USING(artist_id)
+        GROUP BY T.title, A.name
+        ORDER BY copies_sold DESC
+        LIMIT %s
+        ;
+        """
+
+    with _conn.cursor() as cur:
+        cur.execute(query, (n,))
+        data = cur.fetchall()
+
+    return pd.DataFrame(data)
+
+
+def get_popular_albums(_conn: Connection, n: int = 5) -> pd.DataFrame:
+    """Returns the N most sold albums in the database."""
+
+    print("Collating most popular albums.")
+
+    query = """
+        SELECT AB.title, AT.name, COUNT(*) AS copies_sold
+        FROM album_purchase AS PA
+        JOIN album AS AB
+        USING(album_id)
+        JOIN artist as AT
+        USING(artist_id)
+        GROUP BY AB.title, AT.name
+        ORDER BY copies_sold DESC
+        LIMIT %s
+        ;
+        """
+
+    with _conn.cursor() as cur:
+        cur.execute(query, (n,))
+        data = cur.fetchall()
+
+    return pd.DataFrame(data)
+
+
+def get_popular_artists(_conn: Connection, n: int = 5) -> pd.DataFrame:
+    """Returns the N artists with the most sales in the database."""
+
+    print("Collating most popular artists.")
+
+    query = """
+        SELECT A.name, COUNT(AP.album_purchase_id)+COUNT(TP.track_purchase_id) AS total_sales
+        FROM artist as A
+        JOIN album AS AB
+        USING(artist_id)
+        JOIN album_purchase AS AP
+        USING(album_id)
+        JOIN track as T
+        USING(artist_id)
+        JOIN track_purchase as TP
+        USING(track_id)
+        GROUP BY AB.title, A.name
+        ORDER BY total_sales DESC
+        LIMIT %s
+        ;
+        """
+
+    with _conn.cursor() as cur:
+        cur.execute(query, (n,))
+        data = cur.fetchall()
+
+    return pd.DataFrame(data)
+
+
+def get_sales_by_tag(_conn: Connection) -> pd.DataFrame:
+    """Returns a count of sales by genre/tag."""
+
+    print("Counting sales by tag.")
+
+    query = """
+        SELECT TG.name AS tag, COUNT(*) AS sales
+        FROM tag AS TG
+        LEFT JOIN album_tag_assignment AS ATG
+        USING(tag_id)
+        LEFT JOIN track_tag_assignment AS TGA
+        USING(tag_id)
+        LEFT JOIN album AS A
+        USING(album_id)
+        LEFT JOIN track AS T
+        USING(track_id)
+        LEFT JOIN album_purchase AS AP
+        USING(album_id)
+        LEFT JOIN track_purchase AS TP
+        USING(track_id)
+        GROUP BY tag
+        ;
+        """
+
+    with _conn.cursor() as cur:
+        cur.execute(query)
+        data = cur.fetchall()
+
+    return pd.DataFrame(data)
+
+
 if __name__ == "__main__":
     load_dotenv()
-    get_connection()
+    conn = get_connection()
+    tracks = get_popular_artists(conn)
+    print(tracks)

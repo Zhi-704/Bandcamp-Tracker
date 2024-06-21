7561dd55fa74b5833bfaf620abc502134069a1b6
Lasped <pedropenelas@hotmail.co.uk>
2024-06-21 14:47:17 +0200

    Created generate_pdf.py script

---
+++ report/generate_pdf.py
@@ -0,0 +1,5 @@
+"""
+This script's purpose is to generate a daily report that will be outputted as a 
+PDF and send it to anyone that is a part of the subscriber list in the RDS used
+for the data obtained from Bandcamp.
+"""

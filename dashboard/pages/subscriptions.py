24099d4c7fdcaf54e9541262629360030ea8e0df
Alina <trainee.alina.tariq@sigmalabs.co.uk>
2024-06-21 09:39:16 +0100

    one combined form

--- dashboard/pages/subscriptions.py
+++ dashboard/pages/subscriptions.py
@@ -11,36 +11,35 @@ if __name__ == "__main__":
     email = ""
     age = ""
     conn = get_connection()
-    col = st.columns(2)
 
-    with col[0]:
-        with st.form("notification-subscription", clear_on_submit=True, border=True):
-            st.header("Subscribe to email notifications by tag")
-            email = st.text_input("Email")
-            if not email:
-                st.error("Missing field - you must enter an 'Email'")
-            if email and "@" not in email:
-                st.error("Invalid email")
+    st.write(
+        "PDF reports: daily summaries of purchases on bandcamp as a PDF emailed to you")
+    st.write("Notifications: ")
+
+    pdf = st.checkbox("I would like to sign up for daily PDF reports")
+    notifications = st.checkbox(
+        "I would like to sign up for notifications for specific tags")
+
+    with st.form("Subscriptions", clear_on_submit=False, border=True):
+        st.header("Subscribe to emails")
+        name = st.text_input("Name")
+        if pdf and not name:
+            st.error("Missing field - you must enter a 'Name'")
+        email = st.text_input("Email")
+        if not email:
+            st.error("Missing field - you must enter an 'Email'")
+        if email and "@" not in email:
+            st.error("Invalid email")
+
+        if notifications:
             tag = st.multiselect(
                 "Choose which tag(s) you would like to subscribe to",
                 get_all_tags(conn),
                 placeholder="Select tag...")
             if not tag:
                 st.error("Missing field - you must choose at least one tag")
-            st.form_submit_button()
+        st.form_submit_button()
 
-        if email and len(tag) == 1:
-            st.write(f"You are now subscribed to the {tag[0]} tag")
+    if email and len(tag) == 1:
+        st.write(f"You are now subscribed to the {tag[0]} tag")
         # add similar message as above for subscribing to multiple tags
-    with col[1]:
-        with st.form("pdf-subscription", clear_on_submit=True, border=True):
-            st.header("Subscribe to daily summary PDF reports")
-            name = st.text_input("Name")
-            if not name:
-                st.error("Missing field - you must enter a 'Name'")
-            email = st.text_input("Email")
-            if not email:
-                st.error("Missing field - you must enter an 'Email'")
-            if email and "@" not in email:
-                st.error("Invalid email")
-            st.form_submit_button()

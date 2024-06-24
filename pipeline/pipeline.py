662d115c217bc652f66d3db16828f71d151138d9
eyuale <157725293+e-lemma@users.noreply.github.com>
2024-06-24 21:31:40 +0100

    create pipeline.py

---
+++ pipeline/pipeline.py
@@ -0,0 +1,8 @@
+"""Main script for running the ETL Pipeline."""
+
+from extract import get_sales_data
+from transform import transform_sales_data
+from load import get_sales_data
+
+if __name__ == "__main__":
+    pass

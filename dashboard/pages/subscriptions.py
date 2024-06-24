16e16f4642fa694df43cbe6a6af9e9d7a234fa23
Alina <trainee.alina.tariq@sigmalabs.co.uk>
2024-06-24 19:43:11 +0100

    sns functionality works

--- dashboard/pages/subscriptions.py
+++ dashboard/pages/subscriptions.py
@@ -1,14 +1,12 @@
 """A dashboard page that lets you sign up for notifications."""
-
-import boto3
-import streamlit as st
-
-
 from database import get_all_tags, get_connection
+import streamlit as st
+import boto3
+from os import environ as ENV
+from dotenv import load_dotenv
+
 
 # Function to create a subscription
-
-
 def create_subscription(protocol, endpoint, topic_arn):
     response = sns_client.subscribe(
         TopicArn=topic_arn,
@@ -19,12 +17,13 @@ def create_subscription(protocol, endpoint, topic_arn):
 
 
 def create_topic(topic_name: str):
-    response = client.create_topic(
+    response = sns_client.create_topic(
         Name=f"c11-bandcamp-{topic_name}-tag",
     )
 
 
 if __name__ == "__main__":
+    load_dotenv()
 
     st.page_link("./Home.py", label="Home")
 
@@ -59,20 +58,34 @@ if __name__ == "__main__":
                 st.error("Missing field - you must choose at least one tag")
         st.form_submit_button()
 
-    if email and len(tag) == 1:
+    if email and len(tags) == 1:
         st.write(f"You are now subscribed to the {tags[0]} tag")
         # add similar message as above for subscribing to multiple tags
 
-    sns_client = boto3.client('sns')
-
-    # Specify the ARN of your SNS topic
     if pdf and name:
-        ...
-        # insert into database
+        with conn.cursor() as cur:
+            cur.execute(
+                """INSERT INTO subscriber (email, name) VALUES (%s, %s)""", (email, name))
+        conn.commit()
+
+    sns_client = boto3.client(
+        'sns',
+        region_name='eu-west-2',
+        aws_access_key_id=ENV["ACCESS_KEY"],
+        aws_secret_access_key=ENV["SECRET_ACCESS_KEY"]
+    )
+    response = sns_client.list_topics()
+    topics = response["Topics"]
+    topic_arns = [topic["TopicArn"] for topic in topics]
+
     if notifications and tags:
         for tag in tags:
-            topic_arn = f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{
-                tag}-tag'
+            if f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{tag}-tag' not in topic_arns:
+                create_topic(tag)
+                create_subscription(
+                    'email', email, f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{tag}-tag')
 
-            # Example: Add an email subscriber
-            email_subscription = create_subscription('email', email, topic_arn)
+            else:
+                topic_arn = f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{
+                    tag}-tag'
+                create_subscription('email', email, topic_arn)

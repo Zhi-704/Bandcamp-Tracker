9cb64cb6b46d274aa84889196c3f2820de87c7d7
Alina <trainee.alina.tariq@sigmalabs.co.uk>
2024-06-24 17:24:39 +0100

    create sns topic function

--- dashboard/pages/subscriptions.py
+++ dashboard/pages/subscriptions.py
@@ -4,6 +4,24 @@ import streamlit as st
 
 from database import get_all_tags, get_connection
 
+# Function to create a subscription
+
+
+def create_subscription(protocol, endpoint, topic_arn):
+    response = sns_client.subscribe(
+        TopicArn=topic_arn,
+        Protocol=protocol,
+        Endpoint=endpoint
+    )
+    return response
+
+
+def create_topic(topic_name: str):
+    response = client.create_topic(
+        Name=f"c11-bandcamp-{topic_name}-tag",
+    )
+
+
 if __name__ == "__main__":
 
     st.page_link("./Home.py", label="Home")
@@ -32,14 +50,28 @@ if __name__ == "__main__":
             st.error("Invalid email")
 
         if notifications:
-            tag = st.multiselect(
+            tags = st.multiselect(
                 "Choose which tag(s) you would like to subscribe to",
                 get_all_tags(conn),
                 placeholder="Select tag...")
-            if not tag:
+            if not tags:
                 st.error("Missing field - you must choose at least one tag")
         st.form_submit_button()
 
     if email and len(tag) == 1:
-        st.write(f"You are now subscribed to the {tag[0]} tag")
+        st.write(f"You are now subscribed to the {tags[0]} tag")
         # add similar message as above for subscribing to multiple tags
+
+    sns_client = boto3.client('sns')
+
+    # Specify the ARN of your SNS topic
+    if pdf and name:
+        ...
+        # insert into database
+    if notifications and tags:
+        for tag in tags:
+            topic_arn = f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{
+                tag}-tag'
+
+            # Example: Add an email subscriber
+            email_subscription = create_subscription('email', email, topic_arn)

d145074a1f04474f353b988d289337b6d898f06f
eyuale <157725293+e-lemma@users.noreply.github.com>
2024-06-25 11:19:11 +0100

    Refactor pipeline script and add testing

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,24 +1,37 @@
 """Main script for running the ETL Pipeline."""
 
+import logging
 from dotenv import load_dotenv
 from extract import get_sales_data
 from transform import transform_sales_data
 from load import load_sales_data
-import logging
-import time
 
-logging.basicConfig(level=logging.INFO)
+
+def main() -> None:
+    """
+    Main function to execute the ETL (Extract, Transform, Load) pipeline.
+
+    - Configures logging with a warning level and a specific format.
+    - Loads environment variables from a .env file using dotenv.
+    - Fetches sales data using get_sales_data().
+    - Transforms the fetched data using transform_sales_data().
+    - Loads the transformed data into a database using load_sales_data().
+    """
+
+    logging.basicConfig(
+        level=logging.WARNING, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    try:
+        load_dotenv()
+
+        list_of_sales = get_sales_data()
+        cleaned_sales = transform_sales_data(list_of_sales)
+        load_sales_data(cleaned_sales)
+
+    except Exception as e:
+        logging.error("An error occurred during ETL pipeline execution: %s", e)
+
 
 if __name__ == "__main__":
-
-    while True:
-        try:
-
-            load_dotenv()
-
-            list_of_sales = get_sales_data()
-            cleaned_sales = transform_sales_data(list_of_sales)
-            load_sales_data(cleaned_sales)
-        except Exception as e:
-            logging.error("An error occurred during the ETL process: %s", e)
-        time.sleep(120)  # Sleep for 2 minutes
+    main()

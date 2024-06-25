60d3d2105c6191ae03aadc893f7eb5c1735be289
Alina <trainee.alina.tariq@sigmalabs.co.uk>
2024-06-25 09:59:28 +0100

    rejig code

--- dashboard/pages/subscriptions.py
+++ dashboard/pages/subscriptions.py
@@ -28,8 +28,7 @@ if __name__ == "__main__":
     st.page_link("./Home.py", label="Home")
 
     conn = get_connection()
-    email = None
-    tags = None
+
     st.write("We offer two different subscriptions:")
     st.write(
         "1. PDF reports - daily summaries of purchases on Bandcamp as a PDF emailed to you")
@@ -41,7 +40,10 @@ if __name__ == "__main__":
     notifications = st.checkbox(
         "I would like to sign up for notifications for specific tags")
 
-    with st.form("Subscriptions", clear_on_submit=False, border=True):
+    email = None
+    tags = None
+
+    with st.form("Subscriptions", clear_on_submit=False, border=True, ):
         st.header("Subscribe to emails")
         if pdf:
             name = st.text_input("Name")
@@ -60,17 +62,7 @@ if __name__ == "__main__":
                 placeholder="Select tag...")
             if not tags:
                 st.error("Missing field - you must choose at least one tag")
-        st.form_submit_button()
-
-    if email and tags:
-        st.write(f"You are now subscribed to the {tags} tag(s)")
-
-    if pdf and name:
-        with conn.cursor() as cur:
-            cur.execute(
-                """INSERT INTO subscriber (email, name) VALUES (%s, %s)""", (email, name))
-        conn.commit()
-        st.write("You are now subscribed to receive daily PDF reports")
+        submitted = st.form_submit_button()
 
     sns_client = boto3.client(
         'sns',
@@ -82,15 +74,24 @@ if __name__ == "__main__":
     topics = response["Topics"]
     topic_arns = [topic["TopicArn"] for topic in topics]
 
-    if notifications and tags and email:
-        for tag in tags:
-            tag = tag.replace(" ", "-")
-            if f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{tag}' not in topic_arns:
-                create_topic(tag)
-                create_subscription(
-                    'email', email, f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{tag}')
+    if submitted:
+        if notifications and email and tags:
+            st.write(f"You are now subscribed to the {tags} tag(s)")
+            for tag in tags:
+                tag = tag.replace(" ", "-")
+                if f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{tag}' not in topic_arns:
+                    create_topic(tag)
+                    create_subscription(
+                        'email', email, f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{tag}')
 
-            else:
-                topic_arn = f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{
-                    tag}'
-                create_subscription('email', email, topic_arn)
+                else:
+                    topic_arn = f'arn:aws:sns:eu-west-2:129033205317:c11-bandcamp-{
+                        tag}'
+                    create_subscription('email', email, topic_arn)
+
+        if pdf and name and email:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """INSERT INTO subscriber (email, name) VALUES (%s, %s)""", (email, name))
+            conn.commit()
+            st.write("You are now subscribed to receive daily PDF reports")

ef963141cb4eb4b07ffdbd15866822ac50df8a2e
Zhi-704 <zhi.liang.704@gmail.com>
2024-06-25 10:16:06 +0100

    Merged multiple items to one email to avoid spam

--- reporting/report.py
+++ reporting/report.py
@@ -1,21 +1,17 @@
 from os import environ as ENV
 from dotenv import load_dotenv
 import pandas as pd
+import logging
 
 from psycopg import connect, Connection
 from psycopg.rows import dict_row
 from boto3 import client
 
-'''
-it sends messages to the topic
-use a boto3 function to retrieve a function of all those tags
-Iterate over those tags to see an album/track is trending under that tag (NO ARTIST)
-Could do that they subscribe to an artist?
-If they are, push a message to the topic
-'''
+# Implement time interval
 
 TRENDING_THRESHOLD = 100
-FILTER_GROUP = "c11-apollo-"
+FILTER_TOPICS_BY = "c11-apollo-"
+TRENDING_TIMEFRAME = '8 hours'
 
 
 def get_connection() -> Connection:
@@ -39,11 +35,11 @@ def get_sns_client() -> client:
 
 
 def filter_topics(filter_word: str, topic_list: list[dict]) -> list[dict]:
-    '''Filters list of topics with relevant topics'''
+    '''Filters list of topics with relevant topics that have a specific word'''
     return [topic for topic in topic_list if filter_word in topic['TopicArn']]
 
 
-def get_list_of_tags_arn(sns_client: client) -> list[dict]:
+def get_topics_arns_from_aws(sns_client: client) -> list[dict]:
     '''Gets all available topics on AWS'''
     response = sns_client.list_topics()
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
@@ -51,28 +47,14 @@ def get_list_of_tags_arn(sns_client: client) -> list[dict]:
     return None
 
 
-def publish_to_topic(sns_client: client, topic_arn: str,  tag: str, item_name: str, item_author: str, item_type: str, item_url: str) -> dict:
-    '''Publishes a message to a topic, which sends an email to all subscribers'''
-
-    subject = f"New trending {item_type} for {tag}!"
-    message = f"The {item_type} '{item_name}' by '{
-        item_author}' is currently trending!\n"
-    message += f"Check it out here at: {item_url}"
-
-    response = sns_client.publish(
-        TopicArn=topic_arn,
-        Subject=subject,
-        Message=message,
-    )
-    return response
-
-
 def get_tag_arn(tag: str, arn_list: list[dict]) -> list[str]:
     '''Get the arn of a specific tag if it exists'''
     return [arn['TopicArn'] for arn in arn_list if tag in arn['TopicArn']]
 
 
-def get_sales_data_of_tag(conn: Connection, item_type: str, tag: str, sales_limit: int = TRENDING_THRESHOLD) -> list[dict]:
+def get_sales_data_of_tag(conn: Connection, item_type: str, tag: str, sales_limit: int = TRENDING_THRESHOLD, sales_timeframe: str = TRENDING_TIMEFRAME) -> list[dict]:
+    '''Gets trending sales data'''
+
     query = f'''
 SELECT
     T.{item_type}_id,
@@ -86,21 +68,29 @@ JOIN artist AS A ON T.artist_id = A.artist_id
 JOIN {item_type}_tag_assignment AS TTA ON T.{item_type}_id = TTA.{item_type}_id
 JOIN tag AS TG ON TTA.tag_id = TG.tag_id
 WHERE TG.name = %s
+AND TP.timestamp >= NOW() - INTERVAL %s
 GROUP BY
     T.{item_type}_id,
     T.title,
-    A.name
+    A.name,
+    T.url
 HAVING COUNT(DISTINCT TP.{item_type}_purchase_id) >= %s
 ORDER BY copies_sold DESC
 '''
 
-    with conn.cursor() as cur:
-        cur.execute(query, (tag, sales_limit))
-        trending_data = cur.fetchall()
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query, (tag, sales_timeframe, sales_limit))
+            trending = cur.fetchall()
 
-    if len(trending_data) >= 1:
-        return trending_data
-    return None
+        if len(trending) >= 1:
+            return trending
+    except Exception as e:
+        logging.error(
+            "Error fetching sales data for %s and tag %s: %s", item_type, tag, e)
+        conn.rollback()
+
+    return []
 
 
 def get_all_tags(conn: Connection) -> list[str]:
@@ -114,41 +104,93 @@ def get_all_tags(conn: Connection) -> list[str]:
 
 
 def extract_tag_name(topic_arn: str) -> str:
-    return topic_arn.split('-')[-1]
+    raw_tag = topic_arn.split(':')[-1]
+    tag_parts = raw_tag.split('-')[2:]
+    return ' '.join(tag_parts).lower()
 
 
-if __name__ == "__main__":
-    load_dotenv()
+def configure_log() -> None:
+    '''Configures log output'''
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
 
-    conn = get_connection()
-    sns = get_sns_client()
 
-    topics = get_list_of_tags_arn(sns)
-    tags_list = filter_topics('apollo', topics)
+def publish_list_to_topic(sns_client: client, topic_arn: str,  tag: str, trending_data: list[dict]) -> dict:
+    '''Publishes trending items for a tag to a topic, which sends an email to all subscribers'''
 
+    subject = f"What's trending for {tag}?"
+    message = f"These items are trending for the tag {tag}!\n\n"
+    for item in trending_data:
+        item_name = item['title']
+        item_author = item['band']
+        item_type = item['type'].title()
+        item_url = item['url']
+
+        message += f"{item_type} '{item_name}' by '{
+            item_author}' is currently trending!\n"
+        message += f"Check it out here at: {item_url}\n\n"
+
+    response = sns_client.publish(
+        TopicArn=topic_arn,
+        Subject=subject,
+        Message=message,
+    )
+    return response
+
+
+def add_tags_to_dictionary(tags_list: list[dict]) -> list[dict]:
     for tag_arn in tags_list:
         topic_arn = tag_arn['TopicArn']
         tag_name = extract_tag_name(topic_arn)
         tag_arn['Tag'] = tag_name
+    return tags_list
 
-    print(tags_list)
+
+def get_trending_items(conn: Connection, item_type: str, tag_name: str):
+    trending_items = get_sales_data_of_tag(
+        conn, item_type, tag_name, 3)
+
+    logging.info("Trending %s (%s) : %s", item_type,
+                 tag_name, trending_items)
+
+    for item in trending_items:
+        item['type'] = item_type
+
+    return trending_items
+
+
+if __name__ == "__main__":
+    configure_log()
+    load_dotenv()
+
+    logging.info("Notification started.")
+
+    conn = get_connection()
+    sns = get_sns_client()
+
+    topics = get_topics_arns_from_aws(sns)
+    tags = filter_topics(FILTER_TOPICS_BY, topics)
+
+    tags_list = add_tags_to_dictionary(tags)
 
     for tag_dict in tags_list:
-        trending_albums = get_sales_data_of_tag(
-            conn, 'album', tag_dict['Tag'], 3)
-        trending_tracks = get_sales_data_of_tag(
-            conn, 'track', tag_dict['Tag'], 3)
-        print(f"Trending album ({tag_dict['Tag']}) : ", trending_albums)
-        print(f"Trending track ({tag_dict['Tag']}) : ", trending_tracks)
 
-        if trending_albums:
-            for album in trending_albums:
-                publish_to_topic(sns, tag_dict['TopicArn'],
-                                 tag_dict['Tag'], album['title'], album['band'], 'album', album['url'])
+        trending_albums = get_trending_items(conn, 'album', tag_dict['Tag'])
+        trending_tracks = get_trending_items(conn, 'track', tag_dict['Tag'])
+        trending_data = trending_tracks + trending_albums
 
-        if trending_tracks:
-            for track in trending_tracks:
-                publish_to_topic(sns, tag_dict['TopicArn'],
-                                 tag_dict['Tag'], track['title'], track['band'], 'track', track['url'])
+        if len(trending_data) == 0:
+            logging.info("No trending items found for %s",
+                         tag_dict['Tag'])
+            continue
+        elif len(trending_data) >= 1:
+            publish_list_to_topic(
+                sns, tag_dict['TopicArn'], tag_dict['Tag'].title(), trending_data)
+            logging.info("Email sent for %s",
+                         tag_dict['Tag'])
 
     conn.close()
+
+    logging.info("Notification system ended.")

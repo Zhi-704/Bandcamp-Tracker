9f02700fd6080a475adab1d65d09af908300e928
eyuale <157725293+e-lemma@users.noreply.github.com>
2024-06-27 22:51:29 +0100

    complete ses mail sender

--- pdf_report/mailer.py
+++ pdf_report/mailer.py
@@ -1,49 +1,88 @@
-from dotenv import load_dotenv
-from os import environ as ENV
+"""Script for sending the PDF reports as email attachments using SES."""
+
+import logging
 import os
-import boto3
-from botocore.exceptions import ClientError
+from os import environ as ENV
+from datetime import datetime
+from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
 
-load_dotenv()
+import boto3
+import botocore.exceptions
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+import psycopg2
+import psycopg2.extras
+from psycopg2.extensions import connection as DBConnection, cursor as DBCursor
 
-SENDER = f"Sender Name <{ENV["SES_SENDER"]}>"
-
-RECIPIENTS = [
-    {"email": "trainee.eyuale.lemma@sigmalabs.co.uk", "name": "Eyuale Lemma"},
-]
 
 AWS_REGION = "eu-west-2"
-
-# The subject line for the email.
-SUBJECT = "Apollo - Daily PDF report"
-
 # The full path to the file that will be attached to the email.
 ATTACHMENT = "./daily_report.pdf"
-
 # The character encoding for the email.
 CHARSET = "utf-8"
 
-# Create a new SES resource and specify a region.
-client = boto3.client(
-    "ses",
-    region_name=AWS_REGION,
-    aws_access_key_id=ENV["ACCESS_KEY"],
-    aws_secret_access_key=ENV["SECRET_ACCESS_KEY"],
-)
 
-for recipient in RECIPIENTS:
-    recipient_email = recipient["email"]
-    recipient_name = recipient["name"]
+def create_ses_client(access_key: str, secret_access_key: str) -> boto3.client:
+    """Creates and returns an SES client using AWS access keys."""
 
-    # The email body for recipients with non-HTML email clients.
-    BODY_TEXT = f"""Good morning, {
-        recipient_name},\r\nPlease see the attached file for your daily generated Bandcamp sales report!"""
+    try:
+        return boto3.client(
+            "ses",
+            region_name=AWS_REGION,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_access_key,
+        )
+    except botocore.exceptions.ClientError as e:
+        raise RuntimeError(f"Error creating SES client: {e}") from e
 
-    # The HTML body of the email.
-    BODY_HTML = f"""\
+
+def get_connection() -> DBConnection:
+    """Creates a database session and returns a connection object."""
+
+    return psycopg2.connect(
+        host=ENV["DB_HOST"],
+        port=ENV["DB_PORT"],
+        user=ENV["DB_USER"],
+        password=ENV["DB_PASS"],
+        database=ENV["DB_NAME"],
+    )
+
+
+def get_cursor(connection: DBConnection) -> DBCursor:
+    """Creates and returns a cursor to execute PostgreSQL commands."""
+
+    return connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+def get_recipients(connection: DBConnection) -> list[dict]:
+    """Queries the subscriber table, returning a list of dictionaries
+    containing subscriber emails and names."""
+
+    recipients = []
+    try:
+        with get_cursor(connection) as cursor:
+            cursor.execute("SELECT email, name FROM subscriber")
+            rows = cursor.fetchall()
+            recipients = [{"email": row["email"], "name": row["name"]} for row in rows]
+    except psycopg2.Error as e:
+        logging.error("Error fetching recipients: %s", e)
+    finally:
+        cursor.close()
+    return recipients
+
+
+def send_email(
+    client: boto3.client,
+    sender: str,
+    recipient_email: str,
+    recipient_name: str,
+    subject: str,
+) -> None:
+    """Sends an email with a PDF attachment using Amazon SES."""
+
+    body_html = f"""\
     <html>
     <head></head>
     <body>
@@ -53,59 +92,65 @@ for recipient in RECIPIENTS:
     </html>
     """
 
-    # Create a multipart/mixed parent container.
-    msg = MIMEMultipart("mixed")
-    # Add subject, from and to lines.
-    msg["Subject"] = SUBJECT
-    msg["From"] = SENDER
+    msg = MIMEMultipart()
+    msg["Subject"] = subject
+    msg["From"] = sender
     msg["To"] = recipient_email
 
-    # Create a multipart/alternative child container.
-    msg_body = MIMEMultipart("alternative")
+    msg.attach(MIMEText(body_html, "html"))
 
-    # Encode the text and HTML content and set the character encoding. This step is
-    # necessary if you're sending a message with characters outside the ASCII range.
-    textpart = MIMEText(BODY_TEXT.encode(CHARSET), "plain", CHARSET)
-    htmlpart = MIMEText(BODY_HTML.encode(CHARSET), "html", CHARSET)
-
-    # Add the text and HTML parts to the child container.
-    msg_body.attach(textpart)
-    msg_body.attach(htmlpart)
-
-    # Define the attachment part and encode it using MIMEApplication.
     with open(ATTACHMENT, "rb") as attachment:
-        att = MIMEApplication(attachment.read())
-
-    # Add a header to tell the email client to treat this part as an attachment,
-    # and to give the attachment a name.
-    att.add_header(
-        "Content-Disposition", "attachment", filename=os.path.basename(ATTACHMENT)
-    )
-
-    # Attach the multipart/alternative child container to the multipart/mixed
-    # parent container.
-    msg.attach(msg_body)
-
-    # Add the attachment to the parent container.
-    msg.attach(att)
+        part = MIMEApplication(attachment.read(), _subtype="pdf")
+        part.add_header(
+            "Content-Disposition", "attachment", filename=os.path.basename(ATTACHMENT)
+        )
+        msg.attach(part)
 
     try:
-        # Provide the contents of the email.
         response = client.send_raw_email(
-            Source=SENDER,
+            Source=sender,
             Destinations=[recipient_email],
-            RawMessage={
-                "Data": msg.as_string(),
-            },
+            RawMessage={"Data": msg.as_string()},
         )
-    # Display an error if something goes wrong.
+        logging.info(
+            "Email sent to %s! Message ID: %s", recipient_email, response["MessageId"]
+        )
     except ClientError as e:
-        print(
-            f"Error sending email to {recipient_email}: {
-                e.response['Error']['Message']}"
+        logging.error(
+            "Error sending email to %s: %s",
+            recipient_email,
+            e.response["Error"]["Message"],
         )
-    else:
-        print(
-            f"Email sent to {recipient_email}! Message ID: {
-                response['MessageId']}"
-        )
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    load_dotenv()
+
+    sender = f"Sender Name <{ENV["SES_SENDER"]}>"
+
+    aws_access_key = str(ENV.get("ACCESS_KEY"))
+    aws_secret_access_key = str(ENV.get("SECRET_ACCESS_KEY"))
+
+    ses_client = create_ses_client(aws_access_key, aws_secret_access_key)
+
+    try:
+        conn = get_connection()
+        subscribers = get_recipients(conn)
+    except Exception as e:
+        logging.error("Failed to retrieve recipients: %s", e)
+        raise Exception(f"Error retrieving recipients: {e}") from e
+    finally:
+        if conn:
+            conn.close()
+
+    today_date = datetime.now().strftime("%Y-%m-%d")
+
+    for subscriber in subscribers:
+        subject = f"Apollo - Daily Report for {
+            subscriber['name']} ({today_date})"
+        send_email(ses_client, sender, subscriber["email"], subscriber["name"], subject)

27e174ba9936fe0d49fb818c990286ffab1d7746
eyuale <157725293+e-lemma@users.noreply.github.com>
2024-06-27 10:54:20 +0100

    improve linting score of test_pipeline.py

--- pipeline/test_pipeline.py
+++ pipeline/test_pipeline.py
@@ -1,13 +1,14 @@
 """Tests for the pipeline script."""
 
 from datetime import datetime
-import pytest
 from unittest.mock import patch
+import pytest
 from pipeline import main
 
 
 @pytest.fixture
 def mock_sales_data():
+    """Pytest fixture of mock sales data."""
     return [
         {
             "utc_date": datetime(2024, 6, 30),
@@ -18,7 +19,7 @@ def mock_sales_data():
             "item_description": "Album Title",
             "album_tags": ["tag1", "tag2"],
             "amount_paid_usd": 10.0,
-            "country": "Country A"
+            "country": "Country A",
         },
         {
             "utc_date": datetime(2024, 6, 29),
@@ -31,20 +32,25 @@ def mock_sales_data():
             "album_url": "https://example.com/album",
             "track_tags": ["tag3", "tag4"],
             "amount_paid_usd": 5.0,
-            "country": "Country B"
-        }
+            "country": "Country B",
+        },
     ]
 
 
-@patch('pipeline.get_sales_data')
-@patch('pipeline.transform_sales_data')
-@patch('pipeline.load_sales_data')
-def test_etl_pipeline(mock_load_sales_data, mock_transform_sales_data, mock_get_sales_data, mock_sales_data):
-    """Tests that the E,T, and L functions are all called once."""
+@patch("pipeline.get_sales_data")
+@patch("pipeline.transform_sales_data")
+@patch("pipeline.load_sales_data")
+def test_etl_pipeline(
+    mock_load_sales_data,
+    mock_transform_sales_data,
+    mock_get_sales_data,
+    mock_sales_data,
+):
+    """Tests that the E,T, and L functions are called once."""
     mock_get_sales_data.return_value = mock_sales_data
     mock_transform_sales_data.return_value = mock_sales_data
 
-    main('foo', 'bar')
+    main("foo", "bar")
 
     mock_get_sales_data.assert_called_once()
     mock_transform_sales_data.assert_called_once_with(mock_sales_data)

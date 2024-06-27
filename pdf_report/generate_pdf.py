5870c9c9b69dab624c358c40bdd504d6eaab24cb
Lasped <pedropenelas@hotmail.co.uk>
2024-06-27 14:16:20 +0100

    Pylint 10/10 achieved

--- pdf_report/generate_pdf.py
+++ pdf_report/generate_pdf.py
@@ -1,7 +1,7 @@
 """
 This script's purpose is to generate a daily report that will be outputted as a
-PDF and send it to anyone that is a part of the subscriber list in the RDS used
-for the data obtained from Bandcamp.
+PDF and then to send it to anyone that is a part of the subscriber list in the RDS
+that is used to store the data which is extracted from Bandcamp.
 """
 
 
@@ -13,12 +13,18 @@ from dotenv import load_dotenv
 import altair as alt
 from xhtml2pdf import pisa
 from tabulate import tabulate
-from queries import (get_top_5_artists_world_sales, get_top_5_tags_world_sales, get_top_5_tracks_world_sales,
-                     get_top_5_countries_sales, get_top_5_metrics_in_top_5_countries)
-from charts import get_top_artists_chart, get_top_tags_chart, get_top_tracks_chart
+from queries import (get_top_5_artists_world_sales,
+                     get_top_5_tags_world_sales,
+                     get_top_5_tracks_world_sales,
+                     get_top_5_countries_sales,
+                     get_top_5_metrics_in_top_5_countries)
+from charts import (get_top_artists_chart,
+                    get_top_tags_chart,
+                    get_top_tracks_chart)
 
 
 def get_connection() -> object:
+    """This function uses psycopg2 to create the connection to the RDS"""
     conn = connect(
         host=ENV["DB_ENDPOINT"],
         user=ENV["DB_USER"],
@@ -31,24 +37,36 @@ def get_connection() -> object:
 
 
 def get_cursor(conn: object) -> object:
+    """This function uses the connection object to create a cursor object"""
     cur = conn.cursor()
     return cur
 
 
 def get_top_5_countries(countries: list[tuple]) -> list[str]:
+    """
+    This function iterates through the list of tuples to output
+    a new list that contains the just the country name in descending order
+    which is ranked off of performance by total revenue made.
+    """
     list_of_countries = [c[0] for c in countries]
     return list_of_countries
 
 
 def convert_html_to_pdf(source_html: str, output_filename: str) -> None:
-    """Outputs HTML to a target file."""
+    """Outputs HTML to a target file and stores it as a PDF."""
     with open(output_filename, "w+b") as f:
         pisa_status = pisa.CreatePDF(source_html, dest=f)
 
+    return pisa_status
 
-def create_html_tables(countries: list[str], country_infos: list[list]):
+
+def create_html_tables(countries: list[str], country_infos: list[list]) -> str:
+    """
+    This function takes a list of countries and all their metrics and puts them
+    a table in the form of a HTML script.
+    """
     tables = ""
-    headers = ["Artists", "Tags", "Tracks"]
+    headers = ["Artists", "Tracks", "Tags"]
 
     for i, country in enumerate(countries):
         country_table = tabulate(
@@ -63,6 +81,7 @@ def create_html_tables(countries: list[str], country_infos: list[list]):
 
 
 def create_html_chart(chart: alt.Chart) -> str:
+    """This function takes in altair charts and converts them into HTML """
     with BytesIO() as bs:
         chart.save(bs, format="png")
         bs.seek(0)
@@ -110,9 +129,9 @@ if __name__ == "__main__":
     <h2>Good Morning Subscriber!<br>Today's daily report provides information on worldwide trends and country specific ones too.</h2>
     <h3><br>Top 5 Rankings Worldwide by Total Revenue</h3>
     <div class="charts">
-        <img src="{artists_chart_html}" class="responsive-charts" />
+        <img src="{artists_chart_html}" class="responsive-charts" style="float:left;"/>
+        <img src="{tracks_chart_html}" class="responsive-charts" style="float:right;" />
         <img src="{tags_chart_html}" class="responsive-charts" />
-        <img src="{tracks_chart_html}" class="responsive-charts" />
     </div>
     <h3><br>Top 5 Rankings based on Location by Number of Purchases </3>
     {html_tables}
@@ -120,4 +139,4 @@ if __name__ == "__main__":
 </html>
 """
 
-    convert_html_to_pdf(html_report, ENV["FILENAME"])
+    convert_result = convert_html_to_pdf(html_report, ENV["FILENAME"])
